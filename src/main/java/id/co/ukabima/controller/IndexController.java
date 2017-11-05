8d4536e4d92bad7488364831fb667e520278660b
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-11-05 11:59:55 +0700

    enabled auth in controller

--- src/main/java/id/co/ukabima/controller/IndexController.java
+++ src/main/java/id/co/ukabima/controller/IndexController.java
@@ -1,13 +1,28 @@
 package id.co.ukabima.controller;
 
+import id.co.ukabima.model.security.User;
+import id.co.ukabima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = {"/", "/home"})
     public String homeApplication() {
-        return "pages/index";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated() &&
+                !(authentication instanceof AnonymousAuthenticationToken)) {
+            User user = userService.findByUsername(authentication.getName());
+            return user.isNasabah() ? "pages/nasabah" : "pages/staff";
+        } else
+            return "pages/index";
     }
 }
