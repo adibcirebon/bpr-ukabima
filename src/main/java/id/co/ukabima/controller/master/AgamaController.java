539ec37dfa3ad5049c2e4f33644c59feb2ae36ec
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-11-19 01:14:45 +0700

    update layout form

--- src/main/java/id/co/ukabima/controller/master/AgamaController.java
+++ src/main/java/id/co/ukabima/controller/master/AgamaController.java
@@ -1,15 +1,39 @@
 package id.co.ukabima.controller.master;
 
+import id.co.ukabima.model.master.Agama;
+import id.co.ukabima.service.AgamaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/agama")
 public class AgamaController {
 
+    @Autowired
+    private AgamaService agamaService;
+
     @GetMapping("/list")
     public String list() {
         return "pages/master/agama/index";
+    }
+
+    @GetMapping(value = {"/form", "/form/{id}"})
+    public String form(
+            @PathVariable(required = false, name = "id") Agama agama,
+            Model model) {
+        if (agama == null)
+            agama = new Agama();
+        model.addAttribute("agama", agama);
+        return "pages/master/agama/form";
+    }
+
+    @PostMapping(value = "/submit")
+    public String submit(@Valid @ModelAttribute Agama agama) {
+        this.agamaService.save(agama);
+        return "redirect:/agama/list";
     }
 }
--- src/main/resources/static/js/master/agama.js
+++ src/main/resources/static/js/master/agama.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     grid();
 });
 
+function removed(value) {
+    console.log(value + " selected!");
+}
+
 function grid() {
     $('#listAgama').show();
     if (typeof myTable == 'undefined') {
@@ -30,10 +34,23 @@ function grid() {
                 });
             },
             "aoColumns": [
-                {"mDataProp": "id", "bSortable": false, sClass: "center"},
+                {"mDataProp": "id", "bSortable": false, sClass: "text-center"},
                 {"mDataProp": "nama", "bSortable": true, sDefaultContent: "-"},
-                {"mDataProp": "id", "bSortable": false, sClass: "-"}
-            ]
+                {"mDataProp": "id", "bSortable": false, sClass: "text-right"}
+            ], "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull, oSettings) {
+                var info = this.api().page.info();
+                var page = info.page;
+                var length = info.length;
+                var index = (page * length + (iDisplayIndex + 1));
+                var id = "'" + aData['id'] + "'";
+                $('td:eq(0)', nRow).html(index);
+
+                var edit = "<a href='/agama/form/" + aData['id'] + "' class='btn btn-warning'>Ubah</a>";
+                var remove = '<button class="btn btn-danger" onclick="removed(' + id + ')">Hapus</button>';
+                $('td:eq(2)', nRow).html(edit + " " + remove);
+                // $('td:eq(3)', nRow).html(hapus);
+                return nRow;
+            }
         });
     } else {
         myTable.fnClearTable(0);
